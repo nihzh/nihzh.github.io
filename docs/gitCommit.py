9f83615f86cb3e9110f71ac4aed62f06b86d2d86
nihzh <nihility_e@outlook.com>
2023-07-13 16:35:34 +0800

    20230713-16:35:33

--- docs/gitCommit.py
+++ docs/gitCommit.py
@@ -1,13 +1,16 @@
 import sys
 import os
+import datetime
 
 if __name__ == '__main__':
+    comment = ""
     try:
         comment = sys.argv[1]
-        # os.system("git remote add origin git@github.com:nihzh.github.com.git")
-        os.system("git add .")
-        os.system(f"git commit -m {comment}")
-        os.system("git push -u origin main")
     except Exception as e:
-        print("usage: python gitCommit.py [comment_to_commit]")
+        comment = str(datetime.datetime.now().strftime('%Y%m%d-%H:%M:%S'))
+        print(f'No argument, the comment will be "{comment}"')
+    # os.system("git remote add origin git@github.com:nihzh.github.com.git")
+    os.system("git add .")
+    os.system(f"git commit -m {comment}")
+    os.system("git push -u origin main")
     print("Commit finished")
