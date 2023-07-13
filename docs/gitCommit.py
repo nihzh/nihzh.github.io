8908ad9da406a40585bfc3df90d2351ce3cd9206
nihzh <nihility_e@outlook.com>
2023-07-13 17:18:00 +0800

    fixed commit

--- docs/gitCommit.py
+++ docs/gitCommit.py
@@ -4,9 +4,11 @@ import datetime
 
 if __name__ == '__main__':
     comment = ""
-    for i in sys.argv[1:]:
-        comment += i + " "
-    # comment = str(datetime.datetime.now().strftime('%Y%m%d-%H:%M:%S'))
+    if len(sys.argv) > 1:
+        for i in sys.argv[1:]:
+            comment += i + " "
+    else:
+        comment = str(datetime.datetime.now().strftime('%Y%m%d-%H:%M:%S'))
     # os.system("git remote add origin git@github.com:nihzh.github.com.git")
     os.system("git add .")
     os.system(f'git commit -m "{comment}"')
